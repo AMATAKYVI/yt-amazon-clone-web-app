d3503d92bf9335267850a33ebad7cc7df842751f
AMATAKYVI <yviamatak123@gmail.com>
2022-07-06 10:46:58 -0400

    user information page

--- pages/userinformation.js
+++ pages/userinformation.js
@@ -131,9 +131,50 @@ function UserInformation() {
           <div className="text-xl font-semibold ">
             Some Product Recommendations
           </div>
+          <div className="grid grid-cols-3 gap-5">
+            <div className="border border-gray-300 h-[100px] ">Box 1</div>
+            <div className="border border-gray-300  h-[100px] ">Box 2</div>
+            <div className="border border-gray-300 h-[100px] ">Box 3</div>
+            <div className="border border-gray-300 h-[100px] ">Box 4</div>
+            <div className="border border-gray-300 h-[100px] ">Box 5</div>
+          </div>
         </div>
       </div>
-      <div>Footer component here</div>
+      <div className="bg-[#232F3E] text-white min-h-[20vh]">
+        <div className="text-center font-semibold tracking-wide text-xl">
+          Amazon
+        </div>
+        <div className="grid grid-cols-2 items-center gap-2 max-w-[1280px] mx-auto text-center">
+          <div className="text-xs">
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              Contact
+            </div>
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              Sell on Amazon
+            </div>
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              Book store
+            </div>
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              Affiliates
+            </div>
+          </div>
+          <div className="text-xs">
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              About
+            </div>
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              Terms and Policy
+            </div>
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              Return Products
+            </div>
+            <div className="py-2 cursor-pointer tracking-wide w-[60%] hover:bg-gray-200 transition-all duration-500 hover:text-black mx-auto">
+              Warehouses
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
